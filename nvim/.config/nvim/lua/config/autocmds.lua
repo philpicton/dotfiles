8e8b92b2610ddd11ec248f708da411588a96e2db
Phil Picton <hi@philpicton.dev>
2025-12-10 09:31:55 +0000

    lazyvim is treating env files as bash scripts - silence diagnostics but keep syntax highlighting

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local lsp_hacks = vim.api.nvim_create_augroup("LspHacks", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
+  group = lsp_hacks,
+  pattern = ".env*",
+  callback = function(e)
+    vim.diagnostic.enable(false, { bufnr = e.buf })
+  end,
+})
