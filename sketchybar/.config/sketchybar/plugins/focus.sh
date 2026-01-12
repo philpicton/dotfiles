44ae0fb299a36af415870079ae3094ae1d4c73f7
Phil Picton <hi@philpicton.dev>
2026-01-12 22:13:07 +0000

    improve icons for focus display

--- sketchybar/.config/sketchybar/plugins/focus.sh
+++ sketchybar/.config/sketchybar/plugins/focus.sh
@@ -3,11 +3,32 @@
 FOCUS=$(shortcuts run "Get Current Focus" 2>/dev/null)
 
 if [ -z "$FOCUS" ] || [ "$FOCUS" == "null" ]; then
-  ICON=""
-  LABEL=""
+    ICON=""
+    LABEL=""
+elif [ "$FOCUS" == "Personal" ]; then
+    ICON="☢︎"
+    LABEL="$FOCUS"
+elif [ "$FOCUS" == "Work" ]; then
+    ICON="⚒︎"
+    LABEL="$FOCUS"
+elif [ "$FOCUS" == "Sleep" ]; then
+    ICON="☾"
+    LABEL="$FOCUS"
+elif [ "$FOCUS" == "Do Not Disturb" ]; then
+    ICON="󰀝"
+    LABEL="DND"
+elif [ "$FOCUS" == "Driving" ]; then
+    ICON="🚗"
+    LABEL="$FOCUS"
+elif [ "$FOCUS" == "Fitness" ]; then
+    ICON="🏃"
+    LABEL="$FOCUS"
+elif [ "$FOCUS" == "Reduce Interruptions" ]; then
+    ICON="⏣"
+    LABEL="Reduce"
 else
-  ICON="󰀝"
-  LABEL="$FOCUS"
+    ICON="☣︎"
+    LABEL="$FOCUS"
 fi
 
 sketchybar --set "$NAME" icon="$ICON" label="$LABEL"
