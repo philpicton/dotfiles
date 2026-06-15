81bd9a79b0e50074d020dcee9a73ad2ba77d962c
Phil Picton <hi@philpicton.dev>
2026-06-15 23:19:33 +0100

    lsp tweak for tailwind's unknown @ rules which _still_ error in 2026

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -3,9 +3,25 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        cssls = {
+          filetypes = { "css", "scss", "less" },
+        },
         vue_ls = {
+          -- vue_ls owns the embedded HTML/CSS sections for .vue buffers.
           settings = {
             css = {
+              validate = true,
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+            scss = {
+              validate = true,
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+            less = {
               validate = true,
               lint = {
                 unknownAtRules = "ignore",
